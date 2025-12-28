69a4565af2acc8723fe1f63068c43ac0df0ecf76
sohanrambhagatttt81 <sohananuragi81@gmail>
2025-12-28 16:59:03 +0530

    Fix case-sensitive import issue for HealthcareMobileApp

--- src/Pages/solutionRouts/SolutionRouts.jsx
+++ src/Pages/solutionRouts/SolutionRouts.jsx
@@ -1,21 +1,20 @@
-import { children } from "react";
-import  AppLayout  from "../../AppLayout";
+import AppLayout from "../../AppLayout";
 import EducationMobileApp from "./EducationMobileApp";
-import HealthcareMobileApp from "./HealthcareMobileApp";
+import HealthCareMobileApp from "./HealthCareMobileApp";
 import RetailMobileApp from "./RetailMobileApp";
 import FintechMobileApp from "./FintageMobileApp";
 import ElearningMobileApp from "./ElearningMobileApp";
 import SportMobileApp from "./SportsMobileApp";
-import CasinoMobileAppApp from "./CasinoMobileApp";
+import CasinoMobileApp from "./CasinoMobileApp";
 import GamingMobileApp from "./GamingMobileApp";
 import TaxiMobileApp from "./TaxiMobileApp";
 import MarketplaceMobileApp from "./MarketplaceMobileApp";
-import EcommerceMobileApp from "./EcommerceMobileApp";
+import EcommerceMobileApp from "./eCommerceMobileApp";
 import HospitalMobileApp from "./HospitalMobileApp";
 import TravelMobileApp from "./TravelMobileApp";
 import AviationMobileApp from "./AviationMobileApp";
 import FoodMobileApp from "./FoodMobileApp";
-import OnDemandMobileApp from "./OnDemandMobileApp";
+import OnDemandMobileApp from "./onDemandMobileApp";
 import RestaurantMobileApp from "./RestorantMobileApp";
 import JewelleryMobileApp from "./JewelleryMobileApp";
 import EntertainmentApp from "./EntertainmentMobileApp";
@@ -24,33 +23,34 @@ import AgricultureMobileApp from "./AgricultureMobileApp";
 import EventMobileApp from "./EventMobileApp";
 import BankingMobileApp from "./BankingMobileApp";
 
-
 export const solutionRouts = [
-    {path: 'applayout', element:<AppLayout />, children:[
-       {path: 'educationmobileapp', element: <EducationMobileApp />},
-       {path: 'healthcaremobileapp', element: <HealthcareMobileApp />},
-       {path: 'retailmobileapp', element: <RetailMobileApp />},
-       {path: 'fintechmobileapp', element: <FintechMobileApp />},
-       {path: 'elearningmobileapp', element:<ElearningMobileApp />},
-       {path: 'sportsmobileapp', element:<SportMobileApp />},
-       {path: 'casinomobileapp', element:<CasinoMobileAppApp />},
-       {path: 'gamingmobileapp', element:<GamingMobileApp />},
-       {path: 'taximobileapp', element:<TaxiMobileApp />},
-       {path: 'marketplacemobileapp', element:<MarketplaceMobileApp />},
-       {path: 'ecommercemobileapp', element:<EcommerceMobileApp />},
-       {path: 'hospitalmobileapp', element:<HospitalMobileApp />},
-       {path: 'travelmobileapp', element:<TravelMobileApp />},
-       {path: 'aviationmobileapp', element:<AviationMobileApp />},
-       {path: 'foodmobileapp', element:<FoodMobileApp />},
-       {path: 'ondemandmobileapp', element:<OnDemandMobileApp />},
-       {path: 'restaurantmobileapp', element:<RestaurantMobileApp />},
-       {path: 'jewellerymobileapp', element:<JewelleryMobileApp />},
-       {path: 'entertainmentmobileapp', element:<EntertainmentApp />},
-       {path: 'automotivemobileapp', element:<AutomotiveMobileApp />},
-       {path: 'agriculturemobileapp', element:<AgricultureMobileApp />},
-       {path: 'eventmobileapp', element:<EventMobileApp />},
-       {path: 'bankingmobileapp', element:<BankingMobileApp />}
-    ]},
-
-
-]
+  {
+    path: "applayout",
+    element: <AppLayout />,
+    children: [
+      { path: "educationmobileapp", element: <EducationMobileApp /> },
+      { path: "healthcaremobileapp", element: <HealthCareMobileApp /> },
+      { path: "retailmobileapp", element: <RetailMobileApp /> },
+      { path: "fintechmobileapp", element: <FintechMobileApp /> },
+      { path: "elearningmobileapp", element: <ElearningMobileApp /> },
+      { path: "sportsmobileapp", element: <SportMobileApp /> },
+      { path: "casinomobileapp", element: <CasinoMobileApp /> },
+      { path: "gamingmobileapp", element: <GamingMobileApp /> },
+      { path: "taximobileapp", element: <TaxiMobileApp /> },
+      { path: "marketplacemobileapp", element: <MarketplaceMobileApp /> },
+      { path: "ecommercemobileapp", element: <EcommerceMobileApp /> },
+      { path: "hospitalmobileapp", element: <HospitalMobileApp /> },
+      { path: "travelmobileapp", element: <TravelMobileApp /> },
+      { path: "aviationmobileapp", element: <AviationMobileApp /> },
+      { path: "foodmobileapp", element: <FoodMobileApp /> },
+      { path: "ondemandmobileapp", element: <OnDemandMobileApp /> },
+      { path: "restaurantmobileapp", element: <RestaurantMobileApp /> },
+      { path: "jewellerymobileapp", element: <JewelleryMobileApp /> },
+      { path: "entertainmentmobileapp", element: <EntertainmentApp /> },
+      { path: "automotivemobileapp", element: <AutomotiveMobileApp /> },
+      { path: "agriculturemobileapp", element: <AgricultureMobileApp /> },
+      { path: "eventmobileapp", element: <EventMobileApp /> },
+      { path: "bankingmobileapp", element: <BankingMobileApp /> },
+    ],
+  },
+];
