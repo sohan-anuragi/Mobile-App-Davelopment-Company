825bac8534dbcaca3ca8243829c9fb02b1d79364
sohanrambhagatttt81 <sohananuragi81@gmail>
2025-12-28 19:10:46 +0530

    check

--- src/components/header-components/technologybar.jsx
+++ src/components/header-components/technologybar.jsx
@@ -101,7 +101,7 @@ export default function TechnologyBar() {
         },
         {
           text: "Cloud Computing",
-          icon: technologyIcons.CloudComputing.CloudComputing,
+          icon: technologyIcons.CloudComputing,
           link: "/applayout/cloud-computing-development",
         },
       ],
@@ -111,27 +111,29 @@ export default function TechnologyBar() {
   const { toggle2, hoverEnter, hoverLeave } = useContext(HeaderDataContext);
 
   return (
-    <>
-      <div
-        className={styles["drop-box2"]}
-        onMouseEnter={() => hoverEnter(toggle2)}
-        onMouseLeave={() => hoverLeave()}
-      >
-        <div className={styles["drop-box"]}>
-          {technology.map((colum) => (
-            <div className={styles["inner-box"]}>
-              <h3>{colum.heading}</h3>
-              {colum.textIcon.map((ram) => (
-                <a href={ram.link || "#"} className={styles["inner-a"]}>
-                  <span className={styles["icon-span"]}>{ram.icon}</span>
-                  <div className={styles["heading-div"]}>{ram.text}</div>
-                </a>
-              ))}
-            </div>
-          ))}
-        </div>
-        <ContactBox></ContactBox>
+    <div
+      className={styles["drop-box2"]}
+      onMouseEnter={() => hoverEnter(toggle2)}
+      onMouseLeave={() => hoverLeave()}
+    >
+      <div className={styles["drop-box"]}>
+        {technology.map((colum, index) => (
+          <div key={index} className={styles["inner-box"]}>
+            <h3>{colum.heading}</h3>
+            {colum.textIcon.map((ram, idx) => (
+              <Link
+                key={idx}
+                to={ram.link || "#"}
+                className={styles["inner-a"]}
+              >
+                <span className={styles["icon-span"]}>{ram.icon}</span>
+                <div className={styles["heading-div"]}>{ram.text}</div>
+              </Link>
+            ))}
+          </div>
+        ))}
       </div>
-    </>
+      <ContactBox />
+    </div>
   );
 }
