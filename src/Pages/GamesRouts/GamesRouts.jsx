f01e01143cd791f7809e428bd169e8ae9af0de39
sohanrambhagatttt81 <sohananuragi81@gmail>
2025-12-28 17:07:49 +0530

    Fix build error and resolve import path issue

--- src/Pages/GamesRouts/GamesRouts.jsx
+++ src/Pages/GamesRouts/GamesRouts.jsx
@@ -2,20 +2,27 @@ import AppLayout from "../../AppLayout";
 import ActionGameDevelopment from "./ActionGame";
 import AndroidGameDev from "./AndroidGame";
 import EducationalGameDevelopment from "./EducationalGame";
-import MultiplayerGameDevelopment from "./MultiplayerGame";
+import MultiplayerGameDevelopment from "./MultiPlayerGame";
 import IphoneGameDev from "./iPhoneGame";
 import SportGameDev from "./SportsGame";
 
 export const GamesRouts = [
   {
-    path: "/applayout", element: <AppLayout />, children: [
-
-      {path: "action-game-development",element:<ActionGameDevelopment />},
-      {path: "android-game-development",element:<AndroidGameDev />},
-      {path: "educational-game-development",element:<EducationalGameDevelopment />},
-      {path: "multiplayer-games-development",element:<MultiplayerGameDevelopment />},
-      {path: "iphone-game-development",element:<IphoneGameDev />},
-      {path: "sport-game-development",element:<SportGameDev />},
-    ]
-  }
+    path: "/applayout",
+    element: <AppLayout />,
+    children: [
+      { path: "action-game-development", element: <ActionGameDevelopment /> },
+      { path: "android-game-development", element: <AndroidGameDev /> },
+      {
+        path: "educational-game-development",
+        element: <EducationalGameDevelopment />,
+      },
+      {
+        path: "multiplayer-games-development",
+        element: <MultiplayerGameDevelopment />,
+      },
+      { path: "iphone-game-development", element: <IphoneGameDev /> },
+      { path: "sport-game-development", element: <SportGameDev /> },
+    ],
+  },
 ];
--- src/Pages/ServicesRouts/ServicesRouts.jsx
+++ src/Pages/ServicesRouts/ServicesRouts.jsx
@@ -1,4 +1,3 @@
-import { Children } from "react";
 import WebDevelopment from "./WebDevelopment.jsx";
 import AppLayout from "../../AppLayout.jsx";
 import GraphicDesign from "./GraphicDesign.jsx";
@@ -16,20 +15,27 @@ import AngularServices from "./AngularServices.jsx";
 import WordPressServices from "./WordPressServices.jsx";
 
 export const ServicesRouts = [
-    {path: '/applayout', element: <AppLayout />, children:[
-        {path: 'web-development', element: <WebDevelopment />},
-        {path: 'graphic-design', element: <GraphicDesign />},
-        {path: 'oracle-dba', element: <OracleDBA />},
-        {path: 'android-services', element: <AndroidServices />},
-        {path: 'mongodb-development', element: <MongoDBDevelopment />},
-        {path: 'iphone', element: <Iphone></Iphone>},
-        {path: 'flutter-development', element: <FlutterDevelopment />},
-        {path: 'hybrid-development', element: <HybridAppDevelopment />},
-        {path: 'wearable-app-solutions', element: <WearableAppSolutions />},
-        {path: 'PHP-mysql-development', element: <PHPMySQLDevelopment />},
-        {path: 'laravel-web-development', element: <LaravelWebDevelopment />},
-        {path: 'react-web-development', element: <ReactWebDevelopmentServices />},
-        {path: 'angular-js-web-development', element: <AngularServices />},
-        {path: 'wordpress-web-development', element: <WordPressServices />}
-    ]}
-]
+  {
+    path: "/applayout",
+    element: <AppLayout />,
+    children: [
+      { path: "web-development", element: <WebDevelopment /> },
+      { path: "graphic-design", element: <GraphicDesign /> },
+      { path: "oracle-dba", element: <OracleDBA /> },
+      { path: "android-services", element: <AndroidServices /> },
+      { path: "mongodb-development", element: <MongoDBDevelopment /> },
+      { path: "iphone", element: <Iphone></Iphone> },
+      { path: "flutter-development", element: <FlutterDevelopment /> },
+      { path: "hybrid-development", element: <HybridAppDevelopment /> },
+      { path: "wearable-app-solutions", element: <WearableAppSolutions /> },
+      { path: "PHP-mysql-development", element: <PHPMySQLDevelopment /> },
+      { path: "laravel-web-development", element: <LaravelWebDevelopment /> },
+      {
+        path: "react-web-development",
+        element: <ReactWebDevelopmentServices />,
+      },
+      { path: "angular-js-web-development", element: <AngularServices /> },
+      { path: "wordpress-web-development", element: <WordPressServices /> },
+    ],
+  },
+];
